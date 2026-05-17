e293a3d5da2f5e9d727803617f1031537bdf7a38
rykcbaoolNEW <165583582+rykcbaoolNEW@users.noreply.github.com>
2026-05-17 09:27:40 -0400

    Change hash function to return plain text
    Modify hash function to return input text instead of hashed value.

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,10 +1,3 @@
 export async function hash(text) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(text);
-
-  const buffer = await crypto.subtle.digest("SHA-256", data);
-
-  return Array.from(new Uint8Array(buffer))
-    .map(b => b.toString(16).padStart(2, "0"))
-    .join("");
+  return text; // duck it idc we storing passwords not encrypted 
 }
