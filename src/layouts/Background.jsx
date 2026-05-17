b0f5167c4338cffe70fa098010e04524af7dcb0e
rykcbaoolNEW <165583582+rykcbaoolNEW@users.noreply.github.com>
2026-05-17 10:50:07 -0400

    Update Background.jsx

--- src/layouts/Background.jsx
+++ src/layouts/Background.jsx
@@ -1,15 +1,22 @@
 import clsx from "clsx";
 import nav from "../styles/nav.module.css";
 import theme from "../styles/theming.module.css";
-
 import { useEffect } from "react";
 
-useEffect(() => {
-  const style = document.createElement("style");
-  style.innerHTML = backgroundStyle;
-  document.head.appendChild(style);
+export default function Background({ children, backgroundStyle }) {
+  useEffect(() => {
+    const style = document.createElement("style");
+    style.innerHTML = backgroundStyle;
+    document.head.appendChild(style);
 
-  return () => {
-    document.head.removeChild(style);
-  };
-}, []);
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, [backgroundStyle]);
+
+  return (
+    <div className={clsx(nav.nav, theme.background)}>
+      {children}
+    </div>
+  );
+}
